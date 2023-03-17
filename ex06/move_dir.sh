71bdf858b405e7bdeb18f9956eb62a7526fc5987
rossssa <dlekqls6204@naver.com>
2023-03-18 03:17:44 +0900

    [Docs] 과제 ex06~ex07 수정

--- ex06/move_dir.sh
+++ ex06/move_dir.sh
@@ -1,1 +1,7 @@
-mv test.txt ../newdir/
+FILE=test.txt
+if [ -e $FILE ]; 
+then
+	mv test.txt ../newdir/
+else
+	echo "파일이 없는데"
+fi
--- ex07/find_file.sh
+++ ex07/find_file.sh
@@ -1,1 +1,6 @@
-find .. -name "*.sh"
+if [ -d ../../LikeLion-Common-Subject ]; 
+then
+	find .. -name "*.sh"
+else
+	echo "해당 디렉토리가 존재하지 않습니다."
+fi
